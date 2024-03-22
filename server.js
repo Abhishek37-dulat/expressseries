1141aa94a8bead8822809ca601b0219726b79630
Abhishek Dulat <abhishek11906997dulat@gmail.com>
2024-03-22 13:35:59 +0530

    form, body-parser

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = 8000;
+const port = 8001;
 
-app.use((req, res, next) => {
-  console.log("Server 1");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Save</button>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Server 2");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
 app.listen(port, () => {
   console.log("listening to server: ", port);
 });
